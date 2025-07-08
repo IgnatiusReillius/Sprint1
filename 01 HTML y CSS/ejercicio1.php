04bf31b1032584d2f21bd2b5c3ad0f44b824cfcd
IgnatiusReillius <ignatiusreillius@hotmail.com>
2025-07-08 11:54:00 +0200

    html y css nivel 1

---
+++ 01 HTML y CSS/ejercicio1.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Ejercicio 1</title>
+        <link rel="stylesheet" href="ejercicio1.css" text/css">
+        
+    </head>
+
+    <body>
+        <header class="header">
+            <section class="navegation-bar">
+                <nav>Logo</nav>
+                <nav>nav</nav>
+                <nav>social</nav>
+            </section>
+            <section class="banner">
+                <nav>banner</nav>
+            </section>
+        </header>
+        <main class="main-body">
+            <section>
+                <ul>
+                    <li>article</li>
+                    <li>article</li>
+                    <li>article</li>
+                    <li>article</li>
+                </ul>
+                <div>navegation</div>
+            </section>
+            <aside>
+                <ul>
+                    <li>adds</li>
+                    <li>adds</li>
+                </ul>
+            </aside>
+        </main>
+        <footer>
+            <div>footer</div>
+        </footer>
+    </body>
+</html>
