87b7e631e3f65d12a728399a6686d04b37dc4863
IgnatiusReillius <ignatiusreillius@hotmail.com>
2025-07-30 12:34:34 +0200

    Correciones aportadas por Sandra

--- 07 Debug/CheckNote.php
+++ 07 Debug/CheckNote.php
@@ -1,14 +1,25 @@
 <?php
+    declare(strict_types=1);
+
     class CheckNote {
         private $nota;
-        function VerificarNota($nota) : string{
-            if ($nota >= 60) {
+        public function __construct($nota) {
+            $this->nota = $nota;
+        }
+        function VerificarNota() : string {
+            if ($this->nota > 100 || $this->nota < 0) {
+                return "La nota introducida no es correcta.";
+            } 
+            else if ($this->nota >= 60 && $this->nota <= 100) {
                 return "Va a Primera División.";
-            } else if ($nota >= 45 && $nota < 60) {
+            } 
+            else if ($this->nota >= 45 && $this->nota < 60) {
                 return "Va a Segunda División.";
-            } else if ($nota >= 33 && $nota < 45) {
+            } 
+            else if ($this->nota >= 33 && $this->nota < 45) {
                 return "Va a Tercera División.";
-            } else {
+            } 
+            else {
                 return "Va a tener que recuperar.";
             }
         }
--- 07 Debug/CheckNoteTest.php
+++ 07 Debug/CheckNoteTest.php
@@ -5,26 +5,37 @@
     use PHPUnit\Framework\TestCase;
     
     class CheckNoteTest extends TestCase {
-        private $checkNote;
+        private $checkNote1, $checkNote2, $checkNote3, $checkNote4, $checkNote5;
 
         protected function setUp() : void {
-            $this->checkNote = new CheckNote();
+            $this->checkNote1 = new CheckNote(60);
+            $this->checkNote2 = new CheckNote(50);
+            $this->checkNote3 = new CheckNote(40);
+            $this->checkNote4 = new CheckNote(10);
+            $this->checkNote5 = new CheckNote(120);
         }
         protected function tearDown() : void {
-            $this->checkNote = NULL;
+            $this->checkNote1 = NULL;
+            $this->checkNote2 = NULL;
+            $this->checkNote3 = NULL;
+            $this->checkNote4 = NULL;
+            $this->checkNote5 = NULL;
         }
 
         public function testVerificarNotaPrimera() : void {
-            $this->assertEquals("Va a Primera División.", $this->checkNote->verificarNota(60));
+            $this->assertEquals("Va a Primera División.", $this->checkNote1->verificarNota());
         }
         public function testVerificarNotaSegunda() : void {
-            $this->assertEquals("Va a Segunda División.", $this->checkNote->verificarNota(45));
+            $this->assertEquals("Va a Segunda División.", $this->checkNote2->verificarNota());
         }
         public function testVerificarNotaTercera() : void {
-            $this->assertEquals("Va a Tercera División.", $this->checkNote->verificarNota(33));
+            $this->assertEquals("Va a Tercera División.", $this->checkNote3->verificarNota());
         }
         public function testVerificarNotaRecuperar() : void {
-            $this->assertEquals("Va a tener que recuperar.", $this->checkNote->verificarNota(32));
+            $this->assertEquals("Va a tener que recuperar.", $this->checkNote4->verificarNota());
+        }
+        public function testVerificarNotaIncorrecta() : void {
+            $this->assertEquals("La nota introducida no es correcta.", $this->checkNote5->verificarNota());
         }
     }
 ?>
